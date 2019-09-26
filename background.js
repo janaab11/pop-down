372d90889d0d4795e845106e5504137c8798d1ef
Manas <manas.gooner11@gmail.com>
2019-09-26 18:08:28 +0530

    v 0.2
    Moved the fixed list to local storage. Reading and writing using Promises

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function Incognigo(tab){
+function Incogni_go(tab){
     chrome.tabs.remove(tab.id);
     chrome.windows.getAll(function(all_windows){
         let first_incognito=all_windows.find(window=>window.incognito);
@@ -6,35 +6,56 @@ function Incognigo(tab){
             chrome.tabs.create({url:tab.url, windowId:first_incognito.id})
         else
             chrome.windows.create({url:tab.url, incognito:true});
-    }); 
+    });
+    return Promise.resolve('hi this works!'); 
 }
 
 function readUserList(){
-    return ['medium.com','economist.com'];
+    return new Promise(function(resolve,reject){
+        chrome.storage.local.get(['userList'],function(result){
+            let userList=result['userList'];
+            resolve(userList);
+        })
+    });
 }
 
 function matchUserList(url){
-    let userList=readUserList();
-    for (let item of userList){
-        if (url.match(new RegExp(item,'i')))
-            return true;
-    }
-    return false;
+    return new Promise(function(resolve,reject){
+        readUserList()
+        .then(userList=>{
+            first_match=userList.find(item=>
+                url.match(new RegExp(item,'i')));
+            if (first_match)
+                resolve(true);
+            else
+                resolve(false);
+        });
+    })
 }
 
 chrome.runtime.onInstalled.addListener(function(){
     chrome.contextMenus.create({id:'incognigo', title:'Go Incognito', contexts:['all']});
+    chrome.storage.local.set({'userList':['medium.com','economist.com']})
 });
 
 chrome.contextMenus.onClicked.addListener(function(info,tab){
-    Incognigo(tab);
+    Incogni_go(tab);
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if((changeInfo.url)&&(!tab.incognito)){
+        const before = window.performance.now();
         let newUrl=changeInfo.url;
-
-        if (matchUserList(newUrl))
-            Incognigo(tab);
+        matchUserList(newUrl)
+        .then(match=>{
+            if (match){
+                Incogni_go(tab)
+                .then((check)=>{
+                    // alert(check);
+                    // alert(window.performance.now()-before);
+                });
+            }
+            
+        });
     }
 });
